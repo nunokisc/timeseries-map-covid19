4fbde4a8a6ca19bc47a775d34b8a996ce573e67c
kisc <nuno_kisc@hotmail.com>
2020-03-20 20:50:33 +0000

    add API

--- main.js
+++ main.js
@@ -18,11 +18,241 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/:country/confirmed', function (req, res) {
+    if (req.params.country == "all") {
+        res.json(confirmed);
+    }
+    else {
+        let result = []
+        confirmed.forEach(line => {
+            if (line.country.toLowerCase() == req.params.country.toLowerCase()) {
+                result.push(line);
+            }
+        });
+        if (result.length > 0) {
+            res.json(result);
+        }
+        else {
+            res.json({ error: "country doesnt exists" });
+        }
+    }
+});
+app.get('/:country/confirmed/:date/', function (req, res) {
+    let confirmed_tmp = JSON.parse(JSON.stringify(confirmed));
+    let result = []
+    if (req.params.country == "all") {
+        if (dates.includes(req.params.date.replace(/-/g, "/") + "")) {
+            confirmed_tmp.forEach(line => {
+                line.data = line.data[req.params.date.replace(/-/g, "/")]
+                result.push(line);
+            });
+            res.json(result);
+        }
+        else {
+            res.json({ error: "date out of range" });
+        }
+    }
+    else {
+        if (dates.includes(req.params.date.replace(/-/g, "/") + "")) {
+            confirmed_tmp.forEach(line => {
+                if (line.country.toLowerCase() == req.params.country.toLowerCase()) {
+                    line.data = line.data[req.params.date.replace(/-/g, "/")]
+                    result.push(line);
+                }
+            });
+            if (result.length > 0) {
+                res.json(result);
+            }
+            else {
+                res.json({ error: "country doesnt exists" });
+            }
+        }
+        else {
+            res.json({ error: "date out of range" });
+        }
+    }
+
+});
+
+app.get('/:country/deaths', function (req, res) {
+    if (req.params.country == "all") {
+        res.json(deaths);
+    }
+    else {
+        let result = []
+        deaths.forEach(line => {
+            if (line.country.toLowerCase() == req.params.country.toLowerCase()) {
+                result.push(line);
+            }
+        });
+        if (result.length > 0) {
+            res.json(result);
+        }
+        else {
+            res.json({ error: "country doesnt exists" });
+        }
+    }
+});
+
+
+app.get('/:country/deaths/:date/', function (req, res) {
+    let deaths_tmp = JSON.parse(JSON.stringify(deaths));
+    let result = []
+    if (req.params.country == "all") {
+        if (dates.includes(req.params.date.replace(/-/g, "/") + "")) {
+            deaths_tmp.forEach(line => {
+                line.data = line.data[req.params.date.replace(/-/g, "/")]
+                result.push(line);
+            });
+            res.json(result);
+        }
+        else {
+            res.json({ error: "date out of range" });
+        }
+    }
+    else {
+        if (dates.includes(req.params.date.replace(/-/g, "/") + "")) {
+            deaths_tmp.forEach(line => {
+                if (line.country.toLowerCase() == req.params.country.toLowerCase()) {
+                    line.data = line.data[req.params.date.replace(/-/g, "/")]
+                    result.push(line);
+                }
+            });
+            if (result.length > 0) {
+                res.json(result);
+            }
+            else {
+                res.json({ error: "country doesnt exists" });
+            }
+        }
+        else {
+            res.json({ error: "date out of range" });
+        }
+    }
+
+});
+
+app.get('/:country/recovered', function (req, res) {
+    if (req.params.country == "all") {
+        res.json(recovered);
+    }
+    else {
+        let result = []
+        recovered.forEach(line => {
+            if (line.country.toLowerCase() == req.params.country.toLowerCase()) {
+                result.push(line);
+            }
+        });
+        if (result.length > 0) {
+            res.json(result);
+        }
+        else {
+            res.json({ error: "country doesnt exists" });
+        }
+    }
+});
+app.get('/:country/recovered/:date/', function (req, res) {
+    let recovered_tmp = JSON.parse(JSON.stringify(recovered));
+    let result = []
+    if (req.params.country == "all") {
+        if (dates.includes(req.params.date.replace(/-/g, "/") + "")) {
+            recovered_tmp.forEach(line => {
+                line.data = line.data[req.params.date.replace(/-/g, "/")]
+                result.push(line);
+            });
+            res.json(result);
+        }
+        else {
+            res.json({ error: "date out of range" });
+        }
+    }
+    else {
+        if (dates.includes(req.params.date.replace(/-/g, "/") + "")) {
+            recovered_tmp.forEach(line => {
+                if (line.country.toLowerCase() == req.params.country.toLowerCase()) {
+                    line.data = line.data[req.params.date.replace(/-/g, "/")]
+                    result.push(line);
+                }
+            });
+            if (result.length > 0) {
+                res.json(result);
+            }
+            else {
+                res.json({ error: "country doesnt exists" });
+            }
+        }
+        else {
+            res.json({ error: "date out of range" });
+        }
+    }
+});
+
+app.get('/:country/aggregate/', function (req, res) {
+    if (req.params.country == "all") {
+        res.json(results);
+    }
+    else {
+        let result = []
+        results.forEach(line => {
+            if (line.country.toLowerCase() == req.params.country.toLowerCase()) {
+                result.push(line);
+            }
+        });
+        if (result.length > 0) {
+            res.json(result);
+        }
+        else {
+            res.json({ error: "country doesnt exists" });
+        }
+    }
+});
+app.get('/:country/aggregate/:date/', function (req, res) {
+    let results_tmp = JSON.parse(JSON.stringify(results));
+    let result = []
+    if (req.params.country == "all") {
+        if (dates.includes(req.params.date.replace(/-/g, "/") + "")) {
+            results_tmp.forEach(line => {
+                line.data = line.data[req.params.date.replace(/-/g, "/")]
+                result.push(line);
+            });
+            res.json(result);
+        }
+        else {
+            res.json({ error: "date out of range" });
+        }
+    }
+    else {
+        if (dates.includes(req.params.date.replace(/-/g, "/") + "")) {
+            results_tmp.forEach(line => {
+                if (line.country.toLowerCase() == req.params.country.toLowerCase()) {
+                    line.data = line.data[req.params.date.replace(/-/g, "/")]
+                    result.push(line);
+                }
+            });
+            if (result.length > 0) {
+                res.json(result);
+            }
+            else {
+                res.json({ error: "country doesnt exists" });
+            }
+        }
+        else {
+            res.json({ error: "date out of range" });
+        }
+    }
+});
+app.get('/dates', function (req, res) {
+    res.json(dates);
+});
+
+
+
+
 io.on('connection', function (socket) {
     console.log('a user connected');
     socket.join(socket.handshake.query.token);
 
-    io.to(socket.handshake.query.token).emit("data", {dates:dates, results: results});
+    io.to(socket.handshake.query.token).emit("data", { dates: dates, results: results });
 
     socket.on('disconnect', function () {
         console.log('user disconnected');
@@ -31,7 +261,7 @@ io.on('connection', function (socket) {
 
 
 function extract(filename) {
-    const csv = fs.readFileSync(__dirname  + filename);
+    const csv = fs.readFileSync(__dirname + filename);
     const [headers, ...rows] = parse(csv);
     const [province, country, lat, long, ...dates] = headers;
     const countList = []
@@ -66,7 +296,8 @@ const [recovered] = extract(FILENAME_RECOVERED);;
 const results = [];
 
 for (let index = 0; index < confirmed.length; index++) {
-    results[index] = confirmed[index];
+    results[index] = JSON.parse(JSON.stringify(confirmed[index]));
+    //results[index] = confirmed[index];
     dates.forEach(date => {
         results[index].data[date] = {
             confirmed: confirmed[index].data[date],
